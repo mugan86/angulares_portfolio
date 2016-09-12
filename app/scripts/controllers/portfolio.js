88ab6bcdf053a940e3547ef18d4518901c1b4756
Anartz Muxika Ledo <mugan86@gmail.com>
2016-09-12 13:22:19 +0200

    Add new controllers

---
+++ app/scripts/controllers/portfolio.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mugan86App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mugan86App
+ */
+angular.module('mugan86App')
+  .controller('PortfolioCtrl', function ($rootScope) {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    $rootScope.url = 'http://lorempixel.com/output/city-q-c-1920-1920-2.jpg';
+  });
